312bba206add26de896ff43bfeddb94eb563084c
David Ruan <ruanwz@gmail.com>
2014-03-13 10:26:49 +0800

    to support chinese

--- bm/bookmarklet.js
+++ bm/bookmarklet.js
@@ -5,7 +5,7 @@
     window.sq = {};
     window.sq.userId = '--squirtUser--';
     s = document.createElement('script');
-    s.src = 'http://www.squirt.io/bm/squirt.js';
+    s.src = 'http://ruanwz.github.io/squirt/bm/squirt.js';
     s.s = window.location.search;
     s.idx = s.s.indexOf('sq-dev');
     if(s.idx != -1){
